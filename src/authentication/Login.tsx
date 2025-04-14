d34da0f8aeace650d9d7f51c61113b008b41239f
Arsgn <blackbarber1323@gmail.com>
2025-04-14 17:53:50 +0600

    aaaa

---
+++ src/authentication/Login.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { useAuthStore } from "../store/slices/UseAuthSlice";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const login = useAuthStore((state) => state.login);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      await login(email, password);
+    } catch (error: any) {
+      alert(error.message);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          type="password"
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
